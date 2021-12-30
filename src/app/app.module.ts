81425428c44f628e096ad08c57289361eb16e0c6
Jose Ojeda <jlojeda0903@gmail.com>
2021-12-30 00:46:28 -0500

    Clase 54 Agregar Formulario + Submit Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { RegistrarComponent } from './seguridad/registrar/registrar.component';
 import { LoginComponent } from './seguridad/login/login.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -30,7 +31,8 @@ import { LoginComponent } from './seguridad/login/login.component';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule,
   ],
   providers: [LibrosService],
   bootstrap: [AppComponent]
--- src/app/seguridad/registrar/registrar.component.ts
+++ src/app/seguridad/registrar/registrar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.component.html',
-  styleUrls: ['./registrar.component.css']
+  styleUrls: ['./registrar.component.css'],
 })
 export class RegistrarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  registrarUsuario(form: NgForm) {
+    console.log(form);
   }
-
 }
